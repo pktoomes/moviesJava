dc63a1d0f4e59010cb51752e91604af899a01a36
Patrick Toomes <pktoomes@g.cofc.edu>
2017-11-02 22:38:19 -0400

    minor change to var

--- src/main/java/com/example/moviesJava/MovieController.java
+++ src/main/java/com/example/moviesJava/MovieController.java
@@ -2,13 +2,28 @@ package com.example.moviesJava;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.ui.Model;
+
+import java.util.List;
 
 
 @Controller
 public class MovieController {
 
-    @RequestMapping("/")
-    public
+    static final String MOVIE_URL = "https://api.themoviedb.org/3/movie/now_playing";
+
+
+
+    @RequestMapping(path = "/", method = RequestMethod.GET)
+    public String homeMovie(){
+        return "home";
+    }
+
+    @RequestMapping(path = "/now-playing", method = RequestMethod.GET)
+    public String nowPlaying(Model model){
+        List<Movie> movies =
+    }
 
 
 }
